7819fe28808d4e956605f3e4d868f69c5a159c30
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-09-06 13:00:57 +0500

    update github action

--- src/Pages/App/index.js
+++ src/Pages/App/index.js
@@ -4,10 +4,10 @@ import Products from './Products'
 export default function AppMainIndex() {
 
     const [state, setState] = useState({
-        caName: 'mehran', model: '03we', carPrice: '100000 pkr',
-        caName: 'mehran', model: '03we', carPrice: '100000 pkr',
-        caName: 'mehran', model: '03we', carPrice: '100000 pkr',
-        caName: 'mehran', model: '03we', carPrice: '100000 pkr',
+        ID: 1, caName: 'mehran', model: '03wWe', carPrice: '100000 pkr',
+        ID: 2, caName: '123', model: '03WEwe', carPrice: '100000 pkr',
+        ID: 3, caName: 'mehrqwean', model: '03wEe', carPrice: '100000 pkr',
+        ID: 4, caName: 'mehQWran', model: '03wEe', carPrice: '100000 pkr',
     })
 
     return (
