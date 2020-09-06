ee43c790a3057bcd66284a8662e3160e28904a5e
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-09-06 14:19:40 +0500

    Updates

---
+++ precache-manifest.b9b0c6b8f28554cb2cbb4786b9f858d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601a99db13f6e7d065e5e511a4d6a49b",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e04427d373b7b872a459",
+    "url": "/react-tutorial/static/css/main.ae59c376.chunk.css"
+  },
+  {
+    "revision": "0f2024eb1ab2174833aa",
+    "url": "/react-tutorial/static/js/2.5e6f7342.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-tutorial/static/js/2.5e6f7342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04427d373b7b872a459",
+    "url": "/react-tutorial/static/js/main.efcf1810.chunk.js"
+  },
+  {
+    "revision": "ad2b830e2fd5c4c5061a",
+    "url": "/react-tutorial/static/js/runtime-main.472b6719.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
